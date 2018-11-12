053dcfe7aa7adc72da6cd4d8ed59bd2f377a98c4
Joseph Webb <lysacor@gmail.com>
2018-11-11 19:45:17 -0600

    Setup loops and input with input validation.

--- dice.py
+++ dice.py
@@ -1,12 +1,13 @@
 import random
 
-RAN_NUM = int(random.randint(1, 6))
-
 RESPONSE = 'true'
 
 while RESPONSE == 'true':
-    print(RAN_NUM)
-    RESPONSE = RESPONSE.tolower(input("""Roll the dice again? (true/false)"""))
+    print(int(random.randint(1, 6)))
+    RESPONSE = input("""Roll the dice again? (true/false)""")
+    RESPONSE = RESPONSE.lower()
+    FAILCHECK = 0
     if RESPONSE not in ('true', 'false'):
-        RESPONSE = RESPONSE.tolower(input("""Input incorrect try again?
-        (true/false)"""))
+        while RESPONSE not in ('true', 'false'):
+            RESPONSE = input("""Input incorrect try again? (true/false)""")
+            RESPONSE = RESPONSE.lower()
