8e3cc17c20b64378846508bb6caeccac4573316e
Joseph Webb <lysacor@gmail.com>
2018-11-11 19:24:34 -0600

    added conditions and repetition

--- dice.py
+++ dice.py
@@ -1,4 +1,12 @@
 import random
 
-ran_num = int(random.randint(1, 6))
-print(ran_num)
+RAN_NUM = int(random.randint(1, 6))
+
+RESPONSE = 'true'
+
+while RESPONSE == 'true':
+    print(RAN_NUM)
+    RESPONSE = RESPONSE.tolower(input("""Roll the dice again? (true/false)"""))
+    if RESPONSE not in ('true', 'false'):
+        RESPONSE = RESPONSE.tolower(input("""Input incorrect try again?
+        (true/false)"""))
